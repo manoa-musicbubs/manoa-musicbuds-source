d51eaf438ecdbfb1d3955fc3a463f27010c83a76
doyounghi <59491859+doyounghi@users.noreply.github.com>
2020-04-16 05:21:10 -1000

    ISSUE1 && ISSUE2

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
@@ -6,25 +6,10 @@ class Landing extends React.Component {
   render() {
     return (
         <div className='digits-background'>
-        <Grid container stackable centered columns={3}>
+        <Grid container stackable centered columns={1}>
           <Grid.Column textAlign='center'>
-            <Icon size='huge' name='users' inverted/>
-            <Header as='h1' inverted>Multiple Users</Header>\
-            <Header as='h3' inverted>This address book enables any number of users to register and save
-            their business contacts. You can only see the contact you have created.</Header>
-          </Grid.Column>
-
-          <Grid.Column textAlign='center'>
-            <Icon size='huge' name='file alternate' inverted/>
-            <Header as='h1' inverted>Contact Details</Header>\
-            <Header as='h3' inverted>For each contact, you can save their name, address, and phone number.</Header>
-          </Grid.Column>
-
-          <Grid.Column textAlign='center'>
-            <Icon size='huge' name='calendar check' inverted/>
-            <Header as='h1' inverted>Timestamped Notes</Header>\
-            <Header as='h3' inverted>Each time you make contact with a contact, you can write a note that summarizes
-            the conversation. This note is saved along with a timestamp with the contact.</Header>
+            <Icon size='huge' name='play' inverted/>
+            <Header as='h1' inverted >Together, we Better.</Header>
           </Grid.Column>
         </Grid>
         </div>
